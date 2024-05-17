a4bc5c7c6a9a10a34619dda82c88af009409653b
Puttapong <puttapong.lekk@gmail.com>
2024-05-17 14:22:05 +0700

    edit ex-2

--- ex-2.js
+++ ex-2.js
@@ -8,7 +8,7 @@ function atLeastFive(roomNum, array, operation) {
   }
 }
 
-function compareScroe(studentScore) {
+function compareScore(studentScore) {
   let passed = 0;
   for (let score of studentScore) {
     if (score > 70) {
@@ -24,9 +24,9 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result = atLeastFive(1, studentScoresRoom1, compareScroe);
-let scoreRoom2Result = atLeastFive(2, studentScoresRoom2, compareScroe);
-let scoreRoom3Result = atLeastFive(3, studentScoresRoom3, compareScroe);
+let scoreRoom1Result = atLeastFive(1, studentScoresRoom1, compareScore);
+let scoreRoom2Result = atLeastFive(2, studentScoresRoom2, compareScore);
+let scoreRoom3Result = atLeastFive(3, studentScoresRoom3, compareScore);
 
 console.log(scoreRoom1Result);
 console.log(scoreRoom2Result);
