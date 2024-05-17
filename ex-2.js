bf8bfafe3d196df420435459a9bfac1e972ea4d4
Apirak Fakin <apirakfakin@gmail.com>
2024-05-17 14:18:27 +0700

    feat: add ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,31 @@
 //Exercise #2: At Least Five Function
 
-function atLeastFive(array, operation) {
-  // Start coding here
-}
-
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
+function atLeastFive(classNumber, array, operation, ) {
+  let passCount = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (operation(array[i])) {
+      passCount++;
+    }
+  }
+  let passResult = passCount >= 5;
+  if (passResult) {
+    console.log(`นักเรียนห้องที่ ${classNumber} ผ่านเกณฑ์`);
+  } else {
+    console.log(`นักเรียนห้องที่ ${classNumber} ไม่ผ่านเกณฑ์`);
+  }
+  return passResult;
+}
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+atLeastFive(1, studentScoresRoom1, function (score) {
+  return score > 70;
+});
+atLeastFive(2, studentScoresRoom2, function (score) {
+  return score > 70;
+});
+atLeastFive(3, studentScoresRoom3, function (score) {
+  return score > 70;
+});
